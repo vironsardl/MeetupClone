55bd8ec44dd13ad2ff5fcc635b7c585b5b72f11e
Vironsard Vang <vangvironsardw26@gmail.com>
2025-02-15 10:51:39 +0700

    Event details content page

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,11 +1,34 @@
-import { useLocalSearchParams } from 'expo-router';
-import { Text } from 'react-native';
+import dayjs from 'dayjs';
+import { useLocalSearchParams, Stack } from 'expo-router';
+import { View, Text, Image } from 'react-native';
+
 import events from '~/assets/events.json';
 
 export default function EventPage() {
-  const {id} = useLocalSearchParams();
+  const { id } = useLocalSearchParams();
 
   const event = events.find((e) => e.id === id);
 
-  return <Text>Event Title: {event?.title}</Text>;
+  if (!event) {
+    return <Text>Event not found</Text>;
+  }
+
+  return (
+    <View className="flex-1 gap-3 bg-white p-3">
+      <Stack.Screen
+        options={{ title: 'Event', headerBackTitle: 'Back', headerTintColor: 'black' }}
+      />
+
+      <Image source={{ uri: event.image }} className="aspect-video w-full rounded-2xl" />
+      <Text className="text-3xl font-bold" numberOfLines={2}>
+        {event.title}
+      </Text>
+      <Text className="text-lg" numberOfLines={2}>
+        {event.description}
+      </Text>
+      <Text className="text-lg font-semibold uppercase text-amber-800">
+        {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
+      </Text>
+    </View>
+  );
 }
