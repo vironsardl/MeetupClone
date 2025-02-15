d74a1bd83d0735db09753217da449a9f93d6c19d
Vironsard Vang <vangvironsardw26@gmail.com>
2025-02-15 09:36:08 +0700

    Add flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
 import { Stack } from 'expo-router';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, FlatList } from 'react-native';
 
 import events from '~/assets/events.json';
 import EventListItem from '~/components/EventListItem';
-
 
 export default function Events() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
 
-      <EventListItem event={events[1]} />
-      <EventListItem event={events[2]} />
+      <FlatList
+        data={events}
+        renderItem={({ item }) => <EventListItem event={item} />}
+        className="bg-white"
+      />
     </>
   );
 }
